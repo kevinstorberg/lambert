b2cc2fc9827cba62da99678700b36db3c7d4892f
Kevin J. Storberg <kevin@storberg.net>
2019-03-25 13:19:18 -0400

    partially completed users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,56 @@
 class UsersController < ApplicationController
 
   before_action :set_user,      only: @@crud_actions
-  before_action :authoriz_user, only: @@crud_actions
+  before_action :authorize_user, only: @@crud_actions
+
+  def index
+    @users = User.all
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+    if @user.destroy?
+      
+    else
+
+    end
+  end
 
   private
 
+  def authorize_user
+    return redirect_to root_url unless @user.present?
+    authorize @user
+  end
+
+  def set_user
+    @user = params[:id].present? ? User.find(params[:id]) : User.new
+  end
+
   def user_params
-    
+    params.require(:user).permit(
+      :email,
+      :first_name,
+      :id,
+      :last_name,
+      :remember_created_at,
+      :reset_password_sent_at,
+      :reset_password_token,
+      :role,
+      :titles)
   end
 end
